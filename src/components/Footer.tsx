68996231406fadfb9b44de3a19c9110b78484195
JongTakLim <whdxkr0715@gmail.com>
2025-05-11 21:46:50 +0900

    fdfd

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import '../styles/Footer.scss';  // Footer.scss를 상대 경로로 import
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <p>© 2025 국립순천대학교 제21기 국토순례단</p>
+      {/* <p>© 2025 국립순천대학교 제21기 국토순례단</p> */}
     </footer>
   );
 };
